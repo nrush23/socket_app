8ef216bbbcbba187e8ce926c1ff1189719fa32c7
N.R <nrush@drew.edu>
2023-12-27 22:38:00 -0500

    First attempt with React
    Going to proceed with project using React.

--- client.js
+++ client.js
@@ -1,1 +1,16 @@
-alert("hello");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// class Client extends React.Component {
+//     render() {
+//         return (
+//             <div className="index">
+//                 <h1>This is the test client</h1>
+//                 <p>hello</p>
+//             </div>
+//         );
+//     }
+// }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello, World!</h1>)
