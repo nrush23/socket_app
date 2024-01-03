ed57fbf6b056617362ccfd8a6acf66c945959e52
naria <nrush@drew.edu>
2024-01-02 23:20:43 -0500

    Adding get paths to the server and dummy buttons
    Added more paths in preparation of tracking user IDs and names on the server and created dummy buttons to handle this functionality.

--- server/index.js
+++ server/index.js
@@ -4,9 +4,22 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+let users =  new Map();
+
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from Express!" });
+    res.json({ message: "Welcome to the Chatrooms!" });
 });
+
+app.get("/getID", (req, res) => {
+    const length = users.size;
+    users.set(length, "");
+    res.json({message: length});
+})
+
+app.get("/createUser", (req, res) => {
+
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
--- socket_app/src/App.js
+++ socket_app/src/App.js
@@ -5,17 +5,49 @@ import { useEffect, useState } from "react";
 export default function GUI() {
 
   const [test_data, setData] = useState(null);
+  const [userName, setUser] = useState('');
+  const [userId, setID] = useState(null);
 
   useEffect(() => {
     fetch("/api").then((res) => res.json()).then((data) => setData(data.message));
+    fetch("/getID").then((res) => res.json()).then((data) => {
+      setID(data.message);
+    });
   }, []);
+
+  function setUserName(){
+    setUser(prompt(""));
+    // fetch("/createUser", {
+    //   method: "POST",
+    //   body: JSON.stringify({
+    //     userId: userId,
+    //     userName: userName,
+    //   }),
+    //   headers: {
+    //     "Content-type": "application/json; charset=UTF-8"
+    //   }
+    // }).then((res) => res.json()).then((data) => console.log(data));
+  }
 
   return (
     <>
       <div id="chat_display">
         <p id="chat_log">{!test_data ? "Waiting for input" : test_data}</p>
       </div>
+      <div id="button_bar">
+        <button id="set_user" onClick={setUserName}>
+          Set User Name
+        </button>
+        <button>
+          Create Chatroom
+        </button>
+        <button>
+          Join Chatroom
+        </button>
+      </div>
       <div id="input_field">
+        <p>ID is: {userId}</p>
+        <p>{userName}</p>
         <label>
           Write message below:
           <textarea name="input_text" rows={8} cols={100}></textarea>
