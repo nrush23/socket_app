1938b3fcba0621d3f8d71accb337babac16888ae
naria <nrush@drew.edu>
2024-01-03 18:47:56 -0500

    Added functionality to set username
    User can now set their username by clicking "Set Username", entering their name, and then clicking save which sends a post request to the server to update their userId with their specified username. Next TODO is finish similar mechanism for chatrooms and finish the prompts so they close on save and cancel.

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
 
 let users =  new Map();
+let chatrooms = new Map();
 
 app.get("/api", (req, res) => {
     res.json({ message: "Welcome to the Chatrooms!" });
@@ -16,8 +18,14 @@ app.get("/getID", (req, res) => {
     res.json({message: length});
 })
 
-app.get("/createUser", (req, res) => {
+app.post("/createUser", (req, res) => {
+    users.set(req.body.userId, req.body.userName);
+    res.json({message: "Welcome " + users.get(req.body.userId) + " to the Chatroom!"});
+});
 
+app.post("/createRoom", (req, res) => {
+    chatrooms.set(req.body.roomName, new Map().set(req.body.userId, users.get(req.body.userId)));
+    res.json({message: "New room: " + req.body.roomName + " created!"});
 });
 
 
--- socket_app/src/App.js
+++ socket_app/src/App.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
-
+import Popup from "reactjs-popup";
 
 
 export default function GUI() {
 
-  const [test_data, setData] = useState(null);
-  const [userName, setUser] = useState('');
+  const [chat_log, setData] = useState(null);
+  const [userName, setName] = useState(null);
   const [userId, setID] = useState(null);
+  const [chatroom, setRoom] = useState(null);
 
   useEffect(() => {
     fetch("/api").then((res) => res.json()).then((data) => setData(data.message));
@@ -15,44 +16,94 @@ export default function GUI() {
     });
   }, []);
 
-  function setUserName(){
-    setUser(prompt(""));
-    // fetch("/createUser", {
-    //   method: "POST",
-    //   body: JSON.stringify({
-    //     userId: userId,
-    //     userName: userName,
-    //   }),
-    //   headers: {
-    //     "Content-type": "application/json; charset=UTF-8"
-    //   }
-    // }).then((res) => res.json()).then((data) => console.log(data));
+  function setUserName() {
+    const name = document.getElementById("username_text").value;
+    console.log(name);
+    setName(name);
+    fetch("/createUser", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      body: JSON.stringify({
+        userId: userId,
+        userName: name,
+      }),
+    }).then((res) => res.json()).then((data) => {
+      setData(data.message);
+    });
+  }
+
+  function setChatroom() {
+    const name = document.getElementById("chatroom_name").value;
+    console.log("Chatroom: " + name);
+    setChatroom(name);
+
+    fetch("/createRoom", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      body: JSON.stringify({
+        userId: userId,
+        roomName: name,
+      })
+    }).then((res) => res.json()).then((data) => {
+      setData(data.message);
+    });
   }
 
   return (
     <>
       <div id="chat_display">
-        <p id="chat_log">{!test_data ? "Waiting for input" : test_data}</p>
+        <p id="chat_log">{!chat_log ? "Waiting for Server response" : chat_log}</p>
       </div>
       <div id="button_bar">
-        <button id="set_user" onClick={setUserName}>
-          Set User Name
-        </button>
-        <button>
-          Create Chatroom
-        </button>
+        <Popup trigger={<button id="set_user">Set User Name</button>}>
+          <div>
+            <label>
+              Enter your username:
+              <textarea id="username_text" rows={1} cols={23}></textarea>
+            </label>
+            <div>
+              <button onClick={setUserName}>
+                Save
+              </button>
+              <button>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </Popup>
+        <Popup trigger={<button>Create Chatroom</button>}>
+          <div>
+            <label>
+              Enter your Room name:
+              <textarea id="chatroom_name" rows={1} cols={23}></textarea>
+            </label>
+            <div>
+              <button onClick={setChatroom}>
+                Save
+              </button>
+              <button>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </Popup>
         <button>
           Join Chatroom
         </button>
       </div>
       <div id="input_field">
         <p>ID is: {userId}</p>
-        <p>{userName}</p>
+        <p>Username is: {userName}</p>
         <label>
           Write message below:
           <textarea name="input_text" rows={8} cols={100}></textarea>
         </label>
       </div>
+      <button id="submit">Send</button>
     </>
   );
 }
